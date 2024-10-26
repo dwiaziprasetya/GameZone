4a26dfe94fde715fdda577610ef9c5a26a93e5c6
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-10-27 00:21:33 +0700

    Add carousel slider dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gamezone/src/ui/screen/main_screen.dart';
 import 'package:gamezone/src/ui/theme/color.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,27 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Game Zone',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           scaffoldBackgroundColor: backgroundColor,
           appBarTheme: const AppBarTheme(backgroundColor: backgroundColor)),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-              child: Text('Game Zone',
-                  style: TextStyle(
-                      color: onPrimary,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w100))),
-        ),
-        body: const Center(
-          child: Text(
-            'Welcome, to Game Zone',
-            style: TextStyle(color: onPrimary, fontFamily: 'Poppins'),
-          ),
-        ),
-      ),
+      home: const MainScreen(),
     );
   }
 }
