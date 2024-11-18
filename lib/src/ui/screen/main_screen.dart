d4f148eadbb1ff63d55a81b72533a15f36663661
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-11-18 21:55:19 +0700

    Done with responsive layout in web and android screen

--- lib/src/ui/screen/main_screen.dart
+++ lib/src/ui/screen/main_screen.dart
@@ -6,11 +6,21 @@ import 'package:gamezone/src/ui/widget/image_slider_widget.dart';
 import 'package:gamezone/src/ui/widget/item_game.dart';
 import 'package:gamezone/src/ui/widget/section_title_widget.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final _scrollController1 = ScrollController();
+  final _scrollController2 = ScrollController();
+
+  @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return AnnotatedRegion(
       value: const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
       child: Scaffold(
@@ -53,33 +63,13 @@ class MainScreen extends StatelessWidget {
                       title: 'Popular Games',
                       actionText: 'See all',
                     ),
-                    SizedBox(
-                      height: 230,
-                      width: MediaQuery.of(context).size.width,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: gameModelData.length * 2,
-                        itemBuilder: (context, index) {
-                          final int actualIndex = index % gameModelData.length;
-                          final GameModel game = gameModelData[actualIndex];
-                          return Padding(
-                            padding: const EdgeInsets.only(right: 16),
-                            child: InkWell(
-                              onTap: () {
-                                Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) {
-                                  return DetailScreen(game: game);
-                                }));
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.only(top: 16.0),
-                                child: GameItem(game: game),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
+                    if (screenWidth > 500)
+                      Scrollbar(
+                        controller: _scrollController1,
+                        child: _buildGameList(),
+                      )
+                    else
+                      _buildGameList(),
                     const Padding(
                       padding: EdgeInsets.only(top: 32),
                       child: Center(
@@ -94,87 +84,13 @@ class MainScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 250,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: topGameModelData.length,
-                        itemBuilder: (context, index) {
-                          final GameModel topGame = topGameModelData[index];
-                          return Padding(
-                            padding: const EdgeInsets.only(right: 16),
-                            child: InkWell(
-                              onTap: () {
-                                Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) {
-                                  return DetailScreen(game: topGame);
-                                }));
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.only(top: 32.0),
-                                child: SizedBox(
-                                  width: 300,
-                                  child: Stack(
-                                    children: [
-                                      ClipRRect(
-                                        borderRadius: BorderRadius.circular(10),
-                                        child: Image.asset(
-                                          topGame.imageAsset,
-                                          width: 300,
-                                          height: 250,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                      Positioned(
-                                        bottom: 0,
-                                        left: 0,
-                                        right: 0,
-                                        child: Container(
-                                          padding: const EdgeInsets.all(8),
-                                          decoration: BoxDecoration(
-                                            color:
-                                                Colors.black.withOpacity(0.7),
-                                            borderRadius:
-                                                const BorderRadius.only(
-                                              bottomLeft: Radius.circular(10),
-                                              bottomRight: Radius.circular(10),
-                                            ),
-                                          ),
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                topGame.name,
-                                                style: const TextStyle(
-                                                  fontFamily: 'DMSans',
-                                                  color: Colors.white,
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 16,
-                                                ),
-                                              ),
-                                              const SizedBox(height: 4),
-                                              Text(
-                                                "${topGame.review} reviews",
-                                                style: const TextStyle(
-                                                  fontFamily: 'DMSans',
-                                                  color: Colors.white70,
-                                                  fontSize: 14,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
+                    if (screenWidth > 500)
+                      Scrollbar(
+                        controller: _scrollController2,
+                        child: _buildTopGameList(),
+                      )
+                    else
+                      _buildTopGameList(),
                   ],
                 ),
               ),
@@ -183,5 +99,122 @@ class MainScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _buildGameList() {
+    return SizedBox(
+      height: 230,
+      child: ListView.builder(
+        controller: _scrollController1,
+        scrollDirection: Axis.horizontal,
+        itemCount: gameModelData.length * 2,
+        itemBuilder: (context, index) {
+          final int actualIndex = index % gameModelData.length;
+          final GameModel game = gameModelData[actualIndex];
+          return Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen(game: game);
+                }));
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: GameItem(game: game),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildTopGameList() {
+    return SizedBox(
+      height: 250,
+      child: ListView.builder(
+        controller: _scrollController2,
+        scrollDirection: Axis.horizontal,
+        itemCount: topGameModelData.length,
+        itemBuilder: (context, index) {
+          final GameModel topGame = topGameModelData[index];
+          return Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen(game: topGame);
+                }));
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(top: 32.0),
+                child: SizedBox(
+                  width: 300,
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(
+                          topGame.imageAsset,
+                          width: 300,
+                          height: 250,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.7),
+                            borderRadius: const BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                topGame.name,
+                                style: const TextStyle(
+                                  fontFamily: 'DMSans',
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                "${topGame.review} reviews",
+                                style: const TextStyle(
+                                  fontFamily: 'DMSans',
+                                  color: Colors.white70,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController1.dispose();
+    _scrollController2.dispose();
+    super.dispose();
   }
 }
